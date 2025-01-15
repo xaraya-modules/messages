510426f85f031de6a5a70cc08eb0ddd6d8a62f37
mikespub <github@mikespub.net>
2025-01-15 15:44:55 +0100

    remove dummy main

--- class/usergui.php
+++ class/usergui.php
@@ -32,17 +32,5 @@ sys::import('modules.messages.class.userapi');
  */
 class UserGui extends UserGuiClass
 {
-    /**
-     * User main GUI function
-     * @param array<string, mixed> $args
-     * @return array<mixed>
-     */
-    public function main(array $args = [])
-    {
-        $args['description'] ??= 'Description of messages';
-
-        // Pass along the context for xarTpl::module() if needed
-        $args['context'] ??= $this->getContext();
-        return $args;
-    }
+    // ...
 }
